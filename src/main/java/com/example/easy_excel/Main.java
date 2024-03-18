afb3cff22325c2a93deec8bbe86370a39ee33873
tanxuebin <tanxuebin@4399.com>
2024-03-18 16:42:41 +0800

    feature:添加日志配置

--- src/main/java/com/example/easy_excel/Main.java
+++ src/main/java/com/example/easy_excel/Main.java
@@ -3,11 +3,15 @@ package com.example.easy_excel;
 import com.alibaba.excel.EasyExcel;
 import com.example.easy_excel.easyexcel.ExcelListenter;
 import com.example.easy_excel.vo.H5Data;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
         String inputPath = "D://easy_excel/input/";
         //修改读取对象类H5Data
+        logger.info("读取数据");
         EasyExcel.read(inputPath+"h5.xlsx", H5Data.class,new ExcelListenter()).sheet().doRead();
     }
 }
--- src/main/java/com/example/easy_excel/easyexcel/ExcelListenter.java
+++ src/main/java/com/example/easy_excel/easyexcel/ExcelListenter.java
@@ -2,15 +2,21 @@ package com.example.easy_excel.easyexcel;
 
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
+import com.example.easy_excel.Main;
 import com.example.easy_excel.service.HandleService;
 import com.example.easy_excel.util.OperateFile;
 import com.example.easy_excel.vo.H5Data;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import sun.rmi.runtime.Log;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class ExcelListenter extends AnalysisEventListener<Object> {
+
+    private static final Logger logger = LogManager.getLogger(ExcelListenter.class);
 
     List<Object> list = new ArrayList<Object>();
     private static OperateFile operateFile = new OperateFile();
@@ -30,9 +36,9 @@ public class ExcelListenter extends AnalysisEventListener<Object> {
     @Override
     public void doAfterAllAnalysed(AnalysisContext analysisContext) {
         //这里添加数据处理逻辑
-
-
+        logger.info("处理数据");
         //修改写入对象类H5Data
+        logger.info("写入文件");
         operateFile.writeFile(list,H5Data.class);
     }
 }
